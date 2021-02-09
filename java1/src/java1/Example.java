6cf8c3776df6c2571b39121f8f9cfa9f701b2058
kowsalya30g <kousigowthaman99@gmail.com>
2021-02-09 09:58:11 +0530

    commit

---
+++ java1/src/java1/Example.java
@@ -0,0 +1,11 @@
+package java1;
+
+public class Example {
+	public static void main(String[] args) {
+		System.out.println("mindtree");
+		int a =5;
+		int b = 7;
+		System.out.println(a+b);
+	}
+
+}
