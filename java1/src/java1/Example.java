af60abe9a8176614e95a3fbcac8f36d3e1ecfb8a
kowsalya30g <kousigowthaman99@gmail.com>
2021-02-09 10:05:13 +0530

    commit

--- java1/src/java1/Example.java
+++ java1/src/java1/Example.java
@@ -2,11 +2,11 @@ package java1;
 
 public class Example {
 	public static void main(String[] args) {
-		System.out.println("mindtree");
-		System.out.println("mindtree");
+
 		int a =5;
 		int b = 7;
 		System.out.println(a+b);
+		System.out.println("print");
 	
 		System.out.println("");
 	}
